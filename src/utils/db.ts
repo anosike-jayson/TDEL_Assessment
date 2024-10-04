9f47dce5fea55ff86d6a81a6116c6bc0475fa579
anosike-jayson <kayceeanosike1@gmail.com>
2024-10-04 20:52:35 +0100

    .ENV

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,9 +3,9 @@ import { Client } from 'pg';
 export const client = new Client({
     host: 'localhost',
     port: 5432,
-    user: 'postgres',
-    password:'BUHDEUCe03$',
-    database: 'Trustscrow',
+    user: process.env.PG_USERNAME
+    password: process.env.PG_PASSWORD
+    database: process.env.PG_DATABASE
 });
 
 client.connect();
